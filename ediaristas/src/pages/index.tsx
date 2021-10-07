fa4073d737cf120ee317b5abb1d36399255d81b4
João Pedro <61807534+SystemsAdvanced@users.noreply.github.com>
2021-10-07 13:52:59 -0300

    E-Diaristas

---
+++ ediaristas/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import SafeEnvironment from "../ui/components/feedback/SafeEnvironment/SafeEnvironment";
+
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <SafeEnvironment/>
+    </div>
+  )
+}
+
+export default Home
---
+++ ediaristas/src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import {SafeEnvironmentContainer} from "./SafeEnvironment.style";
+import { Container } from "@mui/material";
+
+const SafeEnvironment = () =>{
+    return (<SafeEnvironmentContainer>
+            <Container>
+                Ambiente Seguro
+            </Container>
+            </SafeEnvironmentContainer>
+    );
+}
+export default SafeEnvironment;
