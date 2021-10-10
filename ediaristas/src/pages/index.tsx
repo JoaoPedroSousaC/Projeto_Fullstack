ba5191c908eec063f8771b621216fa6807a7df0c
João Pedro <61807534+SystemsAdvanced@users.noreply.github.com>
2021-10-10 19:00:22 -0300

    dia 10/10

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -3,16 +3,37 @@ import SafeEnvironment from "../ui/components/feedback/SafeEnvironment/SafeEnvir
 import PageTitle from "../ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "../ui/components/data-display/UserInformation/UserInformation";
 import TextFieldMask from "../ui/components/Inputs/TextFieldMask/TextFieldMask";
+import {Button, Container, Typography} from "@mui/material";
+import {FormElementsContainer, ProfissionaisContainer, ProfissionaisPaper} from "@styles/Page/index.style";
+import useIndex from "../data/hooks/Page/useIndex.page";
 
 const Home: NextPage = () => {
+  const {cep, setCep} = useIndex();
   return (
     <div>
         <SafeEnvironment/>
         <PageTitle title={'Conheça os Profissionais'}
         subtitle={'Preencha seu endereço e conheça os profissionais na sua localidade'}/>
-        <TextFieldMask mask={'99.999-999'} label={`Digite seu CEP`} fullWidth/>
+        <FormElementsContainer>
+            <TextFieldMask mask={'99.999-999'} label={`Digite seu CEP`} fullWidth variant={'outlined'} value = {cep}
+            onChange={(event) => setCep(event.target.value)}/>
 
-        <UserInformation picture={'https://super.abril.com.br/wp-content/uploads/2016/09/super_imggirafa.jpg'} name={'Girafinha'} rating={5} description={'Zoologico'} />
+            <Typography color={`error`}>CEP Inválido</Typography>
+            <Button variant={`contained`} sx={{width:`220px`}}  >Buscar</Button>
+        </FormElementsContainer>
+        <Container>
+            <ProfissionaisPaper>
+                <ProfissionaisContainer>
+                    <UserInformation picture={'https://super.abril.com.br/wp-content/uploads/2016/09/super_imggirafa.jpg'} name={'Girafinha'} rating={5} description={'Zoologico'} />
+                    <UserInformation picture={'https://super.abril.com.br/wp-content/uploads/2016/09/super_imggirafa.jpg'} name={'Girafinha'} rating={5} description={'Zoologico'} />
+                    <UserInformation picture={'https://super.abril.com.br/wp-content/uploads/2016/09/super_imggirafa.jpg'} name={'Girafinha'} rating={5} description={'Zoologico'} />
+                    <UserInformation picture={'https://super.abril.com.br/wp-content/uploads/2016/09/super_imggirafa.jpg'} name={'Girafinha'} rating={5} description={'Zoologico'} />
+                    <UserInformation picture={'https://super.abril.com.br/wp-content/uploads/2016/09/super_imggirafa.jpg'} name={'Girafinha'} rating={5} description={'Zoologico'} />
+                    <UserInformation picture={'https://super.abril.com.br/wp-content/uploads/2016/09/super_imggirafa.jpg'} name={'Girafinha'} rating={5} description={'Zoologico'} />
+                </ProfissionaisContainer>
+            </ProfissionaisPaper>
+        </Container>
+
     </div>
   )
 }
--- ediaristas/src/ui/components/Inputs/TextFieldMask/TextFieldMask.tsx
+++ ediaristas/src/ui/components/Inputs/TextFieldMask/TextFieldMask.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import TextField from "../TextField/TextField";
+// @ts-ignore
 import InputMask from 'react-input-mask';
+import {OutlinedTextFieldProps} from "@mui/material";
 
-export interface TextFieldMaskProps{
+export interface TextFieldMaskProps extends OutlinedTextFieldProps{
     mask: string;
+    value: string;
 }
 
-const TextFieldMask: React.FC<TextFieldMaskProps> = ({mask, ...props}) =>{
+const TextFieldMask: React.FC<TextFieldMaskProps> = ({mask,value, onChange, ...props}) =>{
     return (
-        <InputMask mask={mask}>
+        <InputMask mask={mask} value={value} onChange={onChange}>
             {() => {
                 return <TextField {...props} />;
             }}
