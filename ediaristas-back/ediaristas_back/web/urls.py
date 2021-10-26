5521873998a9ed33499f4c0a90c33423a6172a18
João Pedro <61807534+JoaoPedroSousaC@users.noreply.github.com>
2021-10-26 20:38:13 -0300

    Adicionando funcionalidades ao forms

--- ediaristas-back/ediaristas_back/web/urls.py
+++ ediaristas-back/ediaristas_back/web/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CadastrarDiaristas, listar_diaristas
+from .views import CadastrarDiaristas, listar_diaristas, editar_diarista, remover_diarista
 
-urlpatterns = {
+urlpatterns = [
     path('cadastrar_diarista', CadastrarDiaristas, name='cadastrar_diarista'),
     path('listar_diaristas', listar_diaristas, name='listar_diaristas'),
-}
+    path('editar_diarista/<int:id_diarista>', editar_diarista, name ='editar_diarista'),
+    path('remover_diarista/<int:id_diarista>', remover_diarista, name ='remover_diarista'),
+]
--- ediaristas-back/ediaristas_back/web/views.py
+++ ediaristas-back/ediaristas_back/web/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 from .forms import diarista_form
 
@@ -12,7 +12,7 @@ def CadastrarDiaristas(request):
         form_diarista = diarista_form.DiaristaForm(request.POST, request.FILES)
         if form_diarista.is_valid():
             form_diarista.save()
-            return HttpResponseRedirect('listar_diaristas')
+            return redirect('listar_diaristas')
     else:
         form_diarista = diarista_form.DiaristaForm()
     return render(request, 'forms_diarista.html', {'form_diarista': form_diarista})
@@ -23,3 +23,21 @@ def CadastrarDiaristas(request):
 def listar_diaristas(request):
     diarista = Diarista.objects.all()
     return render(request, 'lista_diaristas.html',{'diaristas': diarista})
+
+def editar_diarista(request, id_diarista):
+    diarista = Diarista.objects.get(id = id_diarista)
+    if request.method == "POST":
+
+        form_diarista = diarista_form.DiaristaForm(request.POST or None, request.FILES, instance=diarista)
+        if form_diarista.is_valid():
+            form_diarista.save()
+            return redirect('listar_diaristas')
+    else:
+        form_diarista = diarista_form.DiaristaForm( instance=diarista)
+
+    return render(request, 'forms_diarista.html',{'form_diarista': form_diarista})
+
+def remover_diarista(request, id_diarista):
+    diarista = Diarista.objects.get(id = id_diarista)
+    diarista.delete()
+    return redirect('listar_diaristas')
